c88fd2ee83f29545dccdf3f2b4beb767da3f5ccb
DerXamtes <ansorg.maximilian@gmail.com>
2024-08-29 04:12:26 +0200

    removed colored blobs becasue thy didn't look good and hurt performance

--- src/app/ram/page.tsx
+++ src/app/ram/page.tsx
@@ -4,17 +4,6 @@ import RAMConfig from "@/components/ram";
 export default function Home() {
   return (
     <main className="relative overflow-hidden bg-background 3xl:px-[10rem] 4xl:px-[20rem]">
-      <div className="animation-delay-2000 absolute left-[28%] top-[60%] h-[45%] w-[70%] animate-blob rounded-full bg-sky-500/30 blur-3xl filter"></div>
-      <div className="animation-delay-8000 absolute left-[10%] top-[57%] h-[30%] w-[60%] animate-blob rounded-full bg-sky-600/40 blur-3xl filter"></div>
-
-      <div className="animation-delay-3000 absolute left-[50%] top-[55%] h-[55%] w-[45%] animate-blob rounded-full bg-cyan-700/20 blur-3xl filter"></div>
-      <div className="animation-delay-9000 absolute left-[25%] top-[68%] h-[35%] w-[80%] animate-blob rounded-full bg-cyan-600/20 blur-3xl filter"></div>
-
-      <div className="animation-delay-2000 absolute left-[18%] top-[27%] h-[25%] w-[55%] animate-blob rounded-full bg-blue-600/30 blur-3xl filter"></div>
-      <div className="animation-delay-1000 absolute left-[57%] top-[39%] h-[35%] w-[40%] animate-blob rounded-full bg-blue-500/30 blur-3xl filter"></div>
-
-      <div className="animation-delay-3000 absolute left-[27%] top-[28%] h-[30%] w-[45%] animate-blob rounded-full bg-indigo-600/20 blur-3xl filter"></div>
-      <div className="animation-delay-7000 absolute left-[17%] top-[8%] h-[25%] w-[55%] animate-blob rounded-full bg-indigo-500/40 blur-3xl filter"></div>
       <div className="relative z-10 flex h-svh flex-col justify-between p-5 sm:p-10 md:pt-20 xl:p-20">
         <div>
           <h1 className="py-5 text-5xl font-semibold sm:text-6xl md:text-7xl lg:py-10 lg:text-8xl xl:text-9xl 2xl:text-[10rem] 3xl:text-[12rem] 4xl:text-[15rem]">
--- src/app/ssd/page.tsx
+++ src/app/ssd/page.tsx
@@ -4,15 +4,6 @@ import SSDConfig from "@/components/ssd";
 export default function Home() {
   return (
     <main className="relative overflow-hidden bg-background 3xl:px-[10rem] 4xl:px-[20rem]">
-      <div className="animation-delay-2000 absolute left-[15%] top-[45%] h-[45%] w-[60%] animate-blob rounded-full bg-amber-500/30 blur-3xl filter"></div>
-      <div className="animation-delay-8000 absolute left-[40%] top-[30%] h-[30%] w-[40%] animate-blob rounded-full bg-amber-700/20 blur-3xl filter"></div>
-      <div className="animation-delay-3000 absolute left-[25%] top-[50%] h-[35%] w-[45%] animate-blob rounded-full bg-amber-700/30 blur-3xl filter"></div>
-      <div className="animation-delay-6000 absolute left-[50%] top-[70%] h-[50%] w-[70%] animate-blob rounded-full bg-amber-800/20 blur-3xl filter"></div>
-
-      <div className="animation-delay-2000 absolute left-[30%] top-[20%] h-[20%] w-[35%] animate-blob rounded-full bg-yellow-600/20 blur-3xl filter"></div>
-      <div className="animation-delay-5000 absolute left-[50%] top-[60%] h-[40%] w-[60%] animate-blob rounded-full bg-yellow-600/20 blur-3xl filter"></div>
-      <div className="animation-delay-1000 absolute left-[65%] top-[40%] h-[25%] w-[50%] animate-blob rounded-full bg-yellow-500/40 blur-3xl filter"></div>
-      <div className="animation-delay-9000 absolute left-[20%] top-[75%] h-[30%] w-[50%] animate-blob rounded-full bg-yellow-500/20 blur-3xl filter"></div>
       <div className="relative z-10 flex h-svh flex-col justify-between p-5 sm:p-10 md:pt-20 xl:p-20">
         <div>
           <h1 className="py-5 text-5xl font-semibold sm:text-6xl md:text-7xl lg:py-10 lg:text-8xl xl:text-9xl 2xl:text-[10rem] 3xl:text-[12rem] 4xl:text-[15rem]">
